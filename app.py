b7fd1ea034087fc8ba043c11e44902419221a784
svantraders-ux <svantraders@gmail.com>
2026-05-17 14:44:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import hashlib
+
+app = Flask(__name__)
+
+VERIFICATION_TOKEN = "inventoryverifytoken123456789"
+
+ENDPOINT = "https://ebay-inventory-webhook.onrender.com"
+
+@app.route("/", methods=["GET", "POST"])
+def webhook():
+
+    challenge_code = request.args.get("challenge_code")
+
+    if challenge_code:
+        hash_input = challenge_code + VERIFICATION_TOKEN + ENDPOINT
+
+        challenge_response = hashlib.sha256(
+            hash_input.encode()
+        ).hexdigest()
+
+        return jsonify({
+            "challengeResponse": challenge_response
+        })
+
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
